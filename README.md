de587bb39b14ea0ff8d9209ee0258e1c83d583b1
Zainul Karim <achmadzainulkarim@gmail.com>
2023-09-08 09:38:48 +0700

    gin init

---
+++ README.md
@@ -0,0 +1,1 @@
+# Go GIN Starter Kit
---
+++ main.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	router := gin.Default()
+	router.GET("/", func(c *gin.Context) {
+		c.JSON(http.StatusOK, gin.H{
+			"name": "jenengku",
+		})
+	})
+	router.Run(":8001") // default 8080
+}
